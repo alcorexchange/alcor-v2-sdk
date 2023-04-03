323402fb3dbfd31d9b4f0e2d2f53dffc0a78f228
Avral <avral@pm.me>
2023-04-03 16:20:33 +0300

    update trade logic & test

--- src/entities/trade.ts
+++ src/entities/trade.ts
@@ -611,13 +611,13 @@ export class Trade<TInput extends Currency, TOutput extends Currency, TTradeType
       // we have arrived at the input token, so this is the first trade of one of the paths
       if (amountIn.currency.equals(tokenIn)) {
         const trade = await Trade.fromRoute(
-            new Route([pool, ...currentPools], currencyIn, currencyAmountOut.currency),
-            currencyAmountOut,
-            TradeType.EXACT_OUTPUT
-          )
+          new Route([pool, ...currentPools], currencyIn, currencyAmountOut.currency),
+          currencyAmountOut,
+          TradeType.EXACT_OUTPUT
+        )
 
         // FIX hotfix, i do not really sure about it
-        if (trade.priceImpact.lessThan(0)) continue
+        if (!trade.inputAmount.greaterThan(0) || trade.priceImpact.lessThan(0)) continue
         //
 
         sortedInsert(
--- test/bestTradeExactOut.test.ts
+++ test/bestTradeExactOut.test.ts
@@ -1,7 +1,9 @@
 import fs from "fs";
+
 import { Pool } from "entities/pool";
 import { Token } from "entities/token";
 import { Trade } from "entities/trade";
+
 import { asset, symbol } from "eos-common";
 import { CurrencyAmount } from "../src/entities/fractions/currencyAmount";
 function parseToken(token) {
@@ -17,9 +19,146 @@ function parseToken(token) {
   );
 }
 
+const toCurrency = (quantity, contract) => {
+  const parsed = asset(quantity)
+  const token = parseToken({ quantity, contract })
+
+  return CurrencyAmount.fromRawAmount(token, parsed.amount.toString())
+}
+
+
 describe("test", () => {
-  const pools: Pool[] = [];
-  const CONTRACT_ONE = "contracta";
+  const poolsData = JSON.parse(
+    fs.readFileSync(__dirname + "/fixtures/pools.json", "utf8")
+  );
+  const ticksData = JSON.parse(
+    fs.readFileSync(__dirname + "/fixtures/ticks.json", "utf8")
+  );
+
+  const pools = poolsData.rows.map((p) => {
+    return new Pool({
+      ...p,
+      tokenA: parseToken(p.tokenA),
+      tokenB: parseToken(p.tokenB),
+      ticks: ticksData[String(p.id)].rows.sort((a, b) => a.id - b.id),
+      sqrtPriceX64: p.currSlot.sqrtPriceX64,
+      tickCurrent: p.currSlot.tick,
+    });
+  });
+
+  // describe("bestTradeExactIn", () => {
+  //   it("test bestTradeExactOut", async () => {
+  //     const token0 = {
+  //       quantity: "100.0000 TLM",
+  //       contract: "alien.worlds",
+  //     };
+  //     const token1 = {
+  //       quantity: "0.00000000 WAX",
+  //       contract: "eosio.token",
+  //     };
+
+  //     const currencyAmountIn = CurrencyAmount.fromRawAmount(
+  //       parseToken(token0),
+  //       100_0000
+  //     );
+
+  //     const [bestExactIn] = await Trade.bestTradeExactIn(
+  //       pools,
+  //       currencyAmountIn,
+  //       parseToken(token1)
+  //     );
+
+  //     const currencyAmountOut = bestExactIn.outputAmount;
+  //     const [bestExactOut] = await Trade.bestTradeExactOut(
+  //       pools,
+  //       parseToken(token0),
+  //       currencyAmountOut
+  //     );
+
+  //     // console.log(
+  //     //   "100.0000 TLM -> ",
+  //     //   bestExactIn.outputAmount.toAsset(),
+  //     //   "| priceImpact:",
+  //     //   bestExactIn.priceImpact.toFixed()
+  //     // );
+
+  //     // console.log(currencyAmountOut.toAsset());
+  //     // console.log(
+  //     //   bestExactOut.outputAmount.toAsset(),
+  //     //   "->",
+  //     //   bestExactOut.inputAmount.toAsset(),
+  //     //   "| priceImpact:",
+  //     //   bestExactOut.priceImpact.toFixed()
+  //     // );
+
+  //     expect(currencyAmountIn.toAsset()).toEqual(bestExactOut.inputAmount.toAsset());
+  //   });
+
+  //   it("test bestTradeExactOut with flip tokens", async () => {
+  //     const poolsData = JSON.parse(
+  //       fs.readFileSync(__dirname + "/fixtures/pools.json", "utf8")
+  //     );
+  //     const ticksData = JSON.parse(
+  //       fs.readFileSync(__dirname + "/fixtures/ticks.json", "utf8")
+  //     );
+
+  //     const pools = poolsData.rows.map((p) => {
+  //       return new Pool({
+  //         ...p,
+  //         tokenA: parseToken(p.tokenA),
+  //         tokenB: parseToken(p.tokenB),
+  //         ticks: ticksData[String(p.id)].rows.sort((a, b) => a.id - b.id),
+  //         sqrtPriceX64: p.currSlot.sqrtPriceX64,
+  //         tickCurrent: p.currSlot.tick,
+  //       });
+  //     });
+
+  //     const token0 = {
+  //       quantity: "0.0000 TLM",
+  //       contract: "alien.worlds",
+  //     };
+
+  //     const token1 = {
+  //       quantity: "0.00000000 WAX",
+  //       contract: "eosio.token",
+  //     };
+
+  //     const currencyAmountIn = CurrencyAmount.fromRawAmount(
+  //       parseToken(token1),
+  //       100_00000000
+  //     );
+
+  //     const [bestExactIn] = await Trade.bestTradeExactIn(
+  //       pools,
+  //       currencyAmountIn,
+  //       parseToken(token0)
+  //     );
+
+  //     const [bestExactOut] = await Trade.bestTradeExactOut(
+  //       pools,
+  //       parseToken(token1),
+  //       bestExactIn.outputAmount
+  //     );
+
+  //     // console.log(
+  //     //   "100.0000 TLM -> ",
+  //     //   bestExactIn.outputAmount.toAsset(),
+  //     //   "| priceImpact:",
+  //     //   bestExactIn.priceImpact.toFixed()
+  //     // );
+
+  //     // console.log(currencyAmountOut.toAsset());
+  //     // console.log(
+  //     //   bestExactOut.outputAmount.toAsset(),
+  //     //   "->",
+  //     //   bestExactOut.inputAmount.toAsset(),
+  //     //   "| priceImpact:",
+  //     //   bestExactOut.priceImpact.toFixed()
+  //     // );
+
+  //     expect(bestExactIn.inputAmount.toAsset()).toEqual(bestExactOut.inputAmount.toAsset());
+  //   });
+  // });
 
   describe("bestTradeExactOut", () => {
     it("test bestTradeExactOut", async () => {
@@ -41,49 +180,87 @@ describe("test", () => {
         });
       });
 
-      const token0 = {
-        quantity: "100.0000 TLM",
-        contract: "alien.worlds",
-      };
-      const token1 = {
-        quantity: "0.00000000 WAX",
-        contract: "eosio.token",
-      };
-
-      const currencyAmountIn = CurrencyAmount.fromRawAmount(
-        parseToken(token0),
-        100_0000
-      );
+      const input = toCurrency('1.00000000 WAX', 'eosio.token')
+      const output = toCurrency('0.0000 TLM', 'alien.worlds')
 
       const [bestExactIn] = await Trade.bestTradeExactIn(
         pools,
-        currencyAmountIn,
-        parseToken(token1)
+        input,
+        output.currency
       );
 
-      // console.log(
-      //   "100.0000 TLM -> ",
-      //   bestExactIn.outputAmount.toAsset(),
-      //   "| priceImpact:",
-      //   bestExactIn.priceImpact.toFixed()
-      // );
-
-      const currencyAmountOut = bestExactIn.outputAmount;
       const [bestExactOut] = await Trade.bestTradeExactOut(
         pools,
-        parseToken(token0),
-        currencyAmountOut
+        input.currency,
+        bestExactIn.outputAmount
       );
-      // console.log(currencyAmountOut.toAsset());
-      // console.log(
-      //   bestExactOut.outputAmount.toAsset(),
-      //   "->",
-      //   bestExactOut.inputAmount.toAsset(),
-      //   "| priceImpact:",
-      //   bestExactOut.priceImpact.toFixed()
-      // );
 
-      expect(currencyAmountIn.toAsset()).toEqual(bestExactOut.inputAmount.toAsset());
+      expect(bestExactOut.inputAmount.toAsset()).toEqual(bestExactIn.inputAmount.toAsset());
     });
+
+    // it("test bestTradeExactOut with flip tokens", async () => {
+    //   const poolsData = JSON.parse(
+    //     fs.readFileSync(__dirname + "/fixtures/pools.json", "utf8")
+    //   );
+    //   const ticksData = JSON.parse(
+    //     fs.readFileSync(__dirname + "/fixtures/ticks.json", "utf8")
+    //   );
+
+    //   const pools = poolsData.rows.map((p) => {
+    //     return new Pool({
+    //       ...p,
+    //       tokenA: parseToken(p.tokenA),
+    //       tokenB: parseToken(p.tokenB),
+    //       ticks: ticksData[String(p.id)].rows.sort((a, b) => a.id - b.id),
+    //       sqrtPriceX64: p.currSlot.sqrtPriceX64,
+    //       tickCurrent: p.currSlot.tick,
+    //     });
+    //   });
+
+    //   const token0 = {
+    //     quantity: "0.0000 TLM",
+    //     contract: "alien.worlds",
+    //   };
+
+    //   const token1 = {
+    //     quantity: "0.00000000 WAX",
+    //     contract: "eosio.token",
+    //   };
+
+    //   const currencyAmountIn = CurrencyAmount.fromRawAmount(
+    //     parseToken(token1),
+    //     100_00000000
+    //   );
+
+    //   const [bestExactIn] = await Trade.bestTradeExactIn(
+    //     pools,
+    //     currencyAmountIn,
+    //     parseToken(token0)
+    //   );
+
+    //   const [bestExactOut] = await Trade.bestTradeExactOut(
+    //     pools,
+    //     parseToken(token1),
+    //     bestExactIn.outputAmount
+    //   );
+
+    //   // console.log(
+    //   //   "100.0000 TLM -> ",
+    //   //   bestExactIn.outputAmount.toAsset(),
+    //   //   "| priceImpact:",
+    //   //   bestExactIn.priceImpact.toFixed()
+    //   // );
+
+    //   // console.log(currencyAmountOut.toAsset());
+    //   // console.log(
+    //   //   bestExactOut.outputAmount.toAsset(),
+    //   //   "->",
+    //   //   bestExactOut.inputAmount.toAsset(),
+    //   //   "| priceImpact:",
+    //   //   bestExactOut.priceImpact.toFixed()
+    //   // );
+
+    //   expect(bestExactIn.inputAmount.toAsset()).toEqual(bestExactOut.inputAmount.toAsset());
+    // });
   });
 });
