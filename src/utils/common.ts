ba24bf4dcf0615d8cc922fcb81726d6086e73542
avral <avral@pm.me>
2024-02-08 16:36:30 +0800

    upd parseTrade method

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,15 +1,17 @@
 import { Percent } from "../entities"
+import { TradeType } from "../internalConstants"
 
 export function parseTrade(trade) {
   // Parse Trade into api format object
-  // 1.0000 EOS
   const slippage = new Percent(3, 100) // 0.3%
-  const receiver = 'myaccount'
+  const receiver = '<receiver>'
+
+  const tradeType = trade.tradeType == TradeType.EXACT_INPUT ? 'swapexactin' : 'swapexactout'
 
   const route = trade.route.pools.map(p => p.id)
   const maxSent = trade.inputAmount
   const minReceived = trade.minimumAmountOut(slippage)
-  const memo = `swapexactin#${route.join(',')}#${receiver}#${minReceived.toExtendedAsset()}#0`
+  const memo = `${tradeType}#${route.join(',')}#${receiver}#${minReceived.toExtendedAsset()}#0`
 
   const result = {
     input: trade.inputAmount.toFixed(),
